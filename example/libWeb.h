a415579c5b23e08592ec5c3a0dbadd54f48396dc
Addy897 <adityasrivastava9943@gmail.com>
2026-06-13 16:36:47 +0530

    Add: better header for usage

---
+++ example/libWeb.h
@@ -0,0 +1,10 @@
+#ifndef LIBWEB_H
+#define LIBWEB_H
+#include "../include/string_view.h"
+#include "../include/json.h"
+#include "../include/hash_table.h"
+#include "../include/request.h"
+#include "../include/response.h"
+#include "../include/routing.h"
+#include "../include/setupServer.h"
+#endif
--- example/main.h
+++ example/main.h
@@ -1,41 +1,4 @@
-#include "string_view.h"
+#include "libWeb.h"
 
 #define getParams(name, params) getHeader(name, params)
-typedef enum { GET = 0, POST = 1, HEAD = 2 } Method;
-struct HashTable;
 
-typedef struct {
-  Method method;
-  StringView version;
-  StringView path;
-  StringView body;
-  struct HashTable *headers;
-  struct HashTable *query_params;
-} Request;
-
-
-typedef struct {
-  int code;
-  const char *message;
-} StatusCode;
-typedef struct {
-  StatusCode status;
-  struct HashTable *headers;
-  char *body;
-} Response;
-
-int initializeSocket();
-void setPublicDir(char *path);
-void setBodyFromFile(char *pathname, Response *res);
-void addRoute(Method m, char *path,
-              void (*callbackfunc)(Request *, Response *));
-void startServer(char *addr, int port);
-void setStatus(int status, Response *response);
-void add_response_header(char *name, char *value, struct HashTable *headers);
-void remove_response_header(char *name, struct HashTable *headers);
-StringView get_response_header(char *name, struct HashTable *headers);
-StringView get_request_header(char *name, struct HashTable *headers);
-int get_all_response_headers(char ** ,struct HashTable *headers);
-void set_response_body(char *body, Response *);
-void set_response_body_sv(StringView body, Response *);
-void cleanupRoutes();
