e82ba2d09e649fc3f16ba5d1d9427b1861161bc6
Addy897 <adityasrivastava9943@gmail.com>
2026-06-15 16:00:29 +0530

    Chnaged sv functions to static inline

--- include/string_view.h
+++ include/string_view.h
@@ -1,37 +1,143 @@
 #ifndef STRING_VIEW_H
 #define STRING_VIEW_H
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
-
+#include <stdint.h>
 
 typedef struct {
-    char * data;
+    const char *data;
     size_t count;
-}StringView;
+} StringView;
 
-#define SV_NULL sv_from_size(NULL,0)
-#define sv_from_cltr(lit) sv_from_size(lit,sizeof(lit)-1)
+#define SV_NULL ((StringView){NULL, 0})
+#define sv_from_cltr(lit) ((StringView){(const char *)(lit), sizeof(lit) - 1})
+
 #define SV_Fmt "%.*s"
-#define SV_Arg(sv) (int) sv.count, sv.data
-#define sv_is_empty(sv) (sv.count < 1 || *(sv.data) == '\0')    
+#define SV_Arg(sv) (int)(sv).count, (sv).data
 
+#define sv_is_empty(sv) ((sv).count == 0)
 
-StringView sv_from_cstr(const char* data);
-StringView sv_from_size(const char* data, size_t n);
-StringView sv_trim_left(StringView sv);
-StringView sv_trim_right(StringView sv);
-StringView sv_trim(StringView sv);
-StringView sv_split(StringView* sv, char delim);
-StringView sv_split_sv(StringView* sv, StringView delim);
-StringView sv_to_lowercase(StringView sv);
+static inline StringView sv_from_cstr(const char *data)
+{
+    return (StringView){ data, data ? strlen(data) : 0 };
+}
 
-int  sv_to_int(StringView sv);
+static inline StringView sv_from_size(const char *data, size_t n)
+{
+    return (StringView){ data, n };
+}
 
-bool sv_eq(StringView left,StringView right);
-bool sv_eq_ignorecase(StringView left,StringView right);
+static inline bool sv_eq(StringView a, StringView b)
+{
+    if (a.count != b.count) return false;
+    if (a.count == 0) return true;
+    return memcmp(a.data, b.data, a.count) == 0;
+}
+
+static inline char sv_to_lower_char(char c)
+{
+    return (c >= 'A' && c <= 'Z') ? (c + 32) : c;
+}
+
+static inline bool sv_eq_ignorecase(StringView a, StringView b)
+{
+    if (a.count != b.count) return false;
+
+    for (size_t i = 0; i < a.count; i++) {
+        if (sv_to_lower_char(a.data[i]) != sv_to_lower_char(b.data[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static inline bool is_space(char c)
+{
+    return (c == ' ' || c == '\t' || c == '\r' ||
+            c == '\n' || c == '\v' || c == '\f');
+}
+
+static inline StringView sv_trim_left(StringView sv)
+{
+    size_t i = 0;
+    while (i < sv.count && is_space(sv.data[i])) {
+        i++;
+    }
+    return sv_from_size(sv.data + i, sv.count - i);
+}
+
+static inline StringView sv_trim_right(StringView sv)
+{
+    size_t i = sv.count;
+    while (i > 0 && is_space(sv.data[i - 1])) {
+        i--;
+    }
+    return sv_from_size(sv.data, i);
+}
+
+static inline StringView sv_trim(StringView sv)
+{
+    return sv_trim_right(sv_trim_left(sv));
+}
+
+static inline StringView sv_split(StringView *sv, char delim)
+{
+    if (!sv->data || sv->count == 0) {
+        return SV_NULL;
+    }
+
+    const char *start = sv->data;
+    const void *p = memchr(start, delim, sv->count);
+
+    size_t idx = p ? (size_t)((const char *)p - start) : sv->count;
+
+    StringView out = { start, idx };
+
+    if (idx < sv->count) {
+        sv->data  += idx + 1;
+        sv->count -= idx + 1;
+    } else {
+        sv->data += idx;
+        sv->count = 0;
+    }
+
+    return out;
+}
+
+static inline StringView sv_split_sv(StringView *sv, StringView delim)
+{
+    if (!sv->data || sv->count == 0 || delim.count == 0) {
+        return SV_NULL;
+    }
+
+    for (size_t i = 0; i + delim.count <= sv->count; i++) {
+        if (sv->data[i] == delim.data[0]) {
+            if (memcmp(sv->data + i, delim.data, delim.count) == 0) {
+                StringView out = { sv->data, i };
+                sv->data  += i + delim.count;
+                sv->count -= i + delim.count;
+                return out;
+            }
+        }
+    }
+
+    StringView out = *sv;
+    sv->data += sv->count;
+    sv->count = 0;
+    return out;
+}
+
+static inline int sv_to_int(StringView sv)
+{
+    int res = 0;
+    for (size_t i = 0; i < sv.count; i++) {
+        char c = sv.data[i];
+        if (c < '0' || c > '9') break;
+        res = res * 10 + (c - '0');
+    }
+    return res;
+}
 
 #endif
-
--- src/string_view.c
+++ src/string_view.c
@@ -3,136 +3,132 @@
 #include "string_view.h"
 
 
-#define  is_digit(x) ('0'<= x && x <= '9')
-#define  is_space(x) (x == ' ' || x == '\t' || x =='\r' || x == '\n' || x == '\v' || x == '\f')
-
-StringView sv_from_cstr(const char* data){
-    StringView sv;
-    sv.data = data;
-    sv.count = strlen(data);
-    return sv;
-}
-
-StringView sv_from_size(const char* data,size_t n){
-    StringView sv;
-    sv.data = data;
-    sv.count = n;
-    return sv;
-
-}
-
-bool sv_eq(StringView left,StringView right){
-     if(left.count != right.count){
-        return false;
-     }else if(left.count ==0){
-        return true;
-    }else{
-        return memcmp(left.data,right.data,left.count) == 0;
-     }
-}
-
-
-StringView sv_trim_left(StringView sv){
-    size_t i = 0;
-    while (i < sv.count && is_space(sv.data[i])) {
-        i += 1;
-    }
-
-    return sv_from_size(sv.data + i, sv.count - i);
-}
-
-StringView sv_trim_right(StringView sv){
-    size_t i = 0;
-    while (i < sv.count && is_space(sv.data[sv.count - 1 - i])) {
-        i += 1;
-    }
-
-    return sv_from_size(sv.data, sv.count - i);
-}
-
-StringView sv_trim(StringView sv){
-    return sv_trim_right(sv_trim_left(sv));
-}
-
-StringView sv_split(StringView * sv, const char delim){
-    if (sv->count == 0 || !sv->data) return sv_from_size(sv->data, 0);
-
-    void *ptr = memchr(sv->data, delim, sv->count);
-    size_t i = ptr ? (size_t)((char*)ptr - sv->data) : sv->count;
-
-    StringView result = sv_from_size(sv->data, i);
-
-    if (i < sv->count) {
-        sv->data  += i + 1;
-        sv->count -= i + 1;
-    } else {
-        sv->data += i;
-        sv->count = 0;
-    }    
-    return result;
-}
-
-StringView sv_split_sv(StringView * sv, StringView delim){
-    if (delim.count == 0 || sv->count == 0 || !sv->data) return sv_from_size(sv->data, 0);
-    
-    size_t i = 0;
-    while (i + delim.count <= sv->count) {
-        void *ptr = memchr(sv->data + i, delim.data[0], sv->count - i - delim.count + 1);
-        if (!ptr) break;
-        
-        i = (size_t)((char*)ptr - sv->data);
-        if (memcmp(sv->data + i, delim.data, delim.count) == 0) {
-            StringView result = sv_from_size(sv->data, i);
-            sv->data  += i + delim.count;
-            sv->count -= i + delim.count;
-            return result;
-        }
-        i++;
-    }
-
-    StringView result = sv_from_size(sv->data, sv->count);
-    sv->data += sv->count;
-    sv->count = 0;
-
-    return result;
-}
-
-bool sv_eq_ignorecase(StringView left,StringView right){
-   if(left.count != right.count){
-        return false;
-    } 
-    char l,r;
-    for(size_t i = 0;i<left.count;i++){
-        if(left.data[i] >= 'A' && left.data[i] <= 'Z'){
-            l = left.data[i] + 32;
-        }else{
-            l = left.data[i];
-        }
-
-        if(right.data[i] >= 'A' && right.data[i] <= 'Z'){
-            r = right.data[i] + 32;
-        }else{
-            r = right.data[i];
-        }
-        if(l != r) return false; 
-    } 
-    return true;
-}
-int sv_to_int(StringView sv){
-    int res = 0;
-    for(int i =0;i<sv.count && is_digit(sv.data[i]);i++){
-            res = res * 10 + (sv.data[i] - '0');
-    }
-    return res;
-        
-}
-
-StringView sv_to_lowercase(StringView sv){
-    if (!sv.data) return sv;
-    for (size_t i = 0; i<sv.count; i++) {
-        if(sv.data[i] >= 'A' && sv.data[i] <= 'Z') {
-            sv.data[i] += 32;         
-        }
-    }
-    return sv; 
-}
+//#define  is_digit(x) ('0'<= x && x <= '9')
+//#define  is_space(x) (x == ' ' || x == '\t' || x =='\r' || x == '\n' || x == '\v' || x == '\f')
+//static inline StringView sv_from_cstr(const char* data){
+//    StringView sv;
+//    sv.data = data;
+//    sv.count = strlen(data);
+//    return sv;
+//}
+//
+//static inline StringView sv_from_size(const char* data,size_t n){
+//    StringView sv;
+//    sv.data = data;
+//    sv.count = n;
+//    return sv;
+//}
+//static inline bool sv_eq(StringView left,StringView right){
+//     if(left.count != right.count){
+//        return false;
+//     }else if(left.count ==0){
+//        return true;
+//    }else{
+//        return memcmp(left.data,right.data,left.count) == 0;
+//     }
+//}
+//
+//StringView sv_trim_left(StringView sv){
+//    size_t i = 0;
+//    while (i < sv.count && is_space(sv.data[i])) {
+//        i += 1;
+//    }
+//
+//    return sv_from_size(sv.data + i, sv.count - i);
+//}
+//
+//StringView sv_trim_right(StringView sv){
+//    size_t i = 0;
+//    while (i < sv.count && is_space(sv.data[sv.count - 1 - i])) {
+//        i += 1;
+//    }
+//
+//    return sv_from_size(sv.data, sv.count - i);
+//}
+//
+//StringView sv_trim(StringView sv){
+//    return sv_trim_right(sv_trim_left(sv));
+//}
+//
+//StringView sv_split(StringView * sv, const char delim){
+//    if (sv->count == 0 || !sv->data) return sv_from_size(sv->data, 0);
+//
+//    void *ptr = memchr(sv->data, delim, sv->count);
+//    size_t i = ptr ? (size_t)((char*)ptr - sv->data) : sv->count;
+//
+//    StringView result = sv_from_size(sv->data, i);
+//
+//    if (i < sv->count) {
+//        sv->data  += i + 1;
+//        sv->count -= i + 1;
+//    } else {
+//        sv->data += i;
+//        sv->count = 0;
+//    }    
+//    return result;
+//}
+//
+//StringView sv_split_sv(StringView * sv, StringView delim){
+//    if (delim.count == 0 || sv->count == 0 || !sv->data) return sv_from_size(sv->data, 0);
+//    
+//    size_t i = 0;
+//    while (i + delim.count <= sv->count) {
+//        void *ptr = memchr(sv->data + i, delim.data[0], sv->count - i - delim.count + 1);
+//        if (!ptr) break;
+//        
+//        i = (size_t)((char*)ptr - sv->data);
+//        if (memcmp(sv->data + i, delim.data, delim.count) == 0) {
+//            StringView result = sv_from_size(sv->data, i);
+//            sv->data  += i + delim.count;
+//            sv->count -= i + delim.count;
+//            return result;
+//        }
+//        i++;
+//    }
+//
+//    StringView result = sv_from_size(sv->data, sv->count);
+//    sv->data += sv->count;
+//    sv->count = 0;
+//
+//    return result;
+//}
+//
+//bool sv_eq_ignorecase(StringView left,StringView right){
+//   if(left.count != right.count){
+//        return false;
+//    } 
+//    char l,r;
+//    for(size_t i = 0;i<left.count;i++){
+//        if(left.data[i] >= 'A' && left.data[i] <= 'Z'){
+//            l = left.data[i] + 32;
+//        }else{
+//            l = left.data[i];
+//        }
+//
+//        if(right.data[i] >= 'A' && right.data[i] <= 'Z'){
+//            r = right.data[i] + 32;
+//        }else{
+//            r = right.data[i];
+//        }
+//        if(l != r) return false; 
+//    } 
+//    return true;
+//}
+//int sv_to_int(StringView sv){
+//    int res = 0;
+//    for(int i =0;i<sv.count && is_digit(sv.data[i]);i++){
+//            res = res * 10 + (sv.data[i] - '0');
+//    }
+//    return res;
+//        
+//}
+//
+//StringView sv_to_lowercase(StringView sv){
+//    if (!sv.data) return sv;
+//    for (size_t i = 0; i<sv.count; i++) {
+//        if(sv.data[i] >= 'A' && sv.data[i] <= 'Z') {
+//            sv.data[i] += 32;         
+//        }
+//    }
+//    return sv; 
+//}
